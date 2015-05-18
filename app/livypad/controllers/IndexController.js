0ada364059dc0d1bd59096fd811ee8482d9538d5
aaronlohwy <aaronlohwy@gmail.com>
2015-05-17 20:20:39 -0500

    updating parse fields for each family member's suggested Appointments

--- app/livypad/controllers/IndexController.js
+++ app/livypad/controllers/IndexController.js
@@ -236,7 +236,11 @@ livypad.controller("IndexController", function($scope,supersonic){
 															 	upperBound : upperBoundAgeString,
 																keyAges: keyAgeString,
 																frequency: frequencyString,
-															});	
+															});
+
+						var famMemberSuggestedAppointmentRelation = famMember.relation("suggestedAppointments");
+						famMemberSuggestedAppointmentRelation.add(famMemberSuggestedAppointment);
+						famMember.save();	
 				  	};
 
 				});
