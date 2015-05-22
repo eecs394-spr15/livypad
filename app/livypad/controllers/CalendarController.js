4d03fb1a4619a2c205566bcf42b016db8cb3480b
aaronlohwy <aaronlohwy@gmail.com>
2015-05-22 00:23:57 -0500

    fixed a bug in the selecting of family members when you create a brand new event
    scheduling from suggested appointments and scheduling completely new
    event now work. Next: scheduling from GCal to LivyPad

--- app/livypad/controllers/CalendarController.js
+++ app/livypad/controllers/CalendarController.js
@@ -82,13 +82,15 @@ livypad.controller("CalendarController", function($scope,supersonic){
 
 
         allFamilyMemberRelations.query().find().then(function(familyMemberResults){
+            $scope.famMemberToAddTo = familyMemberResults[0].id;
             familyMemberResults.forEach(function(famMember){
-                
                 $scope.familyMembersList.push({name: famMember.get("Name"),
                                                 famMember:famMember,
                                                 famMemberID:famMember.id,
                                             });
+
              });
+
         });
         
         //Google Calendar Authorization
