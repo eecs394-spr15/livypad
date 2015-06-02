81bb290c48fc4e0b436781bcf3b97c14c9e1e114
aaronlohwy <aaronlohwy@gmail.com>
2015-06-02 18:06:48 -0500

    changed the form so only the ones Sonia wanted remain

--- app/livypad/controllers/IndexController.js
+++ app/livypad/controllers/IndexController.js
@@ -789,7 +789,7 @@ pushNotification.register(
 	//FUNCTIONS FOR ADDING CUSTOM RECOMMENDED VISIT ///////////////////////////////////////////////////////
 
 	//initializing
-	$scope.newUserAddedRecommendation= {"newTitle": "", "category":"screen", "frequency" : 0, "lower":0, "upper":130, "specialOne":0, "specialTwo":0,"relevantGender":"male"} ;
+	$scope.newUserAddedRecommendation= {"newTitle": "", "category":"screen", "frequency" : 0, "lower":0, "upper":130, "specialOne":0, "specialTwo":0,"relevantGender":"all"} ;
 
 	$scope.addNewRecommendedVisit = function (){
 		var arrayOfSpecialAges = [];
